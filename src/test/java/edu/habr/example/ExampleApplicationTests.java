3f546a6976a5006678180c55d2aa6a18fde77f49
Ilya <iliaaa377@gmail.com>
2023-05-16 17:35:37 +0300

    init

---
+++ src/test/java/edu/habr/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.habr.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
